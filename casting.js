b2d14ca28ef0551d1584d3f908300be93eae2f50
SamylleRose <samylle1brito@gmail.com>
2024-03-14 12:08:03 -0300

    14_03_2024_12:07

---
+++ casting.js
@@ -0,0 +1,17 @@
+let a = prompt("Digite um número")
+let b = prompt("Digite um número")
+
+a = parseInt(a)
+b = parseInt(b)
+
+document.write( a + b)
+
+
+const x = 10
+const y = 20
+
+console.log(x)
+console.log(y)
+
+document.write( x.toString() + y.toString())
+ 
---
+++ if_else.js
@@ -0,0 +1,14 @@
+const a = 4
+const b = 4
+
+
+if (a == b){
+  document.write("Gato preto!")
+
+} else if ( a == 4){
+  document.write("Gato é cinza")
+
+} else {
+  document.write("Gato é branco")
+}
+
